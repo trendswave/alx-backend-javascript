f7c095e280909a8592887e93f09ffcd15c917645
trendswave <paulchinedumicheal@gmail.com>
2024-07-07 14:42:51 +0100

    checked for linting and jest

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,12 +1,27 @@
-export function taskFirst() {
-const task = 'I prefer const when I can.';
-return task;
-export function getLast() {
-return 'is okay';
+/**
+ * Returns a preferred statement about variable declaration.
+ * @returns {string} The statement.
+ */
+export function taskFirst(): string {
+  const task = 'I prefer const when I can.';
+  return task;
 }
-export function taskNext() {
-let combination = 'But sometimes let';
-combination += getLast();
 
-return combination;
+/**
+ * Returns a statement suffix.
+ * @returns {string} The suffix.
+ */
+export function getLast(): string {
+  return ' is okay';
+}
+
+/**
+ * Combines statements about variable declaration preferences.
+ * @returns {string} The combined statement.
+ */
+export function taskNext(): string {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
 }

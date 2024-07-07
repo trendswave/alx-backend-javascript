78b3bc24cb1ceba4a4cd558335d03e61df51b4f8
trendswave <paulchinedumicheal@gmail.com>
2024-07-07 15:37:02 +0100

    function that that uses method property in fullBudget

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);

5f904b752f0dfb5bb6641b425091440c84c9e8ac
trendswave <paulchinedumicheal@gmail.com>
2024-07-07 14:47:03 +0100

    reverted changes

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,27 +1,12 @@
-/**
- * Returns a preferred statement about variable declaration.
- * @returns {string} The statement.
- */
-export function taskFirst(): string {
-  const task = 'I prefer const when I can.';
-  return task;
+export function taskFirst() {
+const task = 'I prefer const when I can.';
+return task;
+export function getLast() {
+return 'is okay';
 }
+export function taskNext() {
+let combination = 'But sometimes let';
+combination += getLast();
 
-/**
- * Returns a statement suffix.
- * @returns {string} The suffix.
- */
-export function getLast(): string {
-  return ' is okay';
-}
-
-/**
- * Combines statements about variable declaration preferences.
- * @returns {string} The combined statement.
- */
-export function taskNext(): string {
-  let combination = 'But sometimes let';
-  combination += getLast();
-
-  return combination;
+return combination;
 }
